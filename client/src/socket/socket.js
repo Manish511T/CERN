eefdb3a7fb931e22761f5e5b0399825772e2c59c
Manish <mkthakur2301@gmail.com>
2026-03-21 16:06:46 +0530

    fix(socket): add polling fallback for production deployement

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client'
 
 const socket = io(import.meta.env.VITE_API_URL?.replace('/api', ''), {
   withCredentials: true,
-  autoConnect: false   // we connect manually after login
+  autoConnect: false,   // we connect manually after login
+  transports: ['websocket', 'polling'],
 })
 
 export default socket;
